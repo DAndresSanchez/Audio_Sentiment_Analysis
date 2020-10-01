10ea1d703debf77ca28f7166c4d59489b44b37d3
David Andres <andressanchezdavid@gmail.com>
2020-10-01 15:13:02 +0100

    Update funtions docstring

--- Audio_Preprocess.py
+++ Audio_Preprocess.py
@@ -5,7 +5,10 @@ import librosa
 import os
 
 # Calculate Energy
-def energy_calc(signal, segment_length):
+def energy_calc(signal: np.array, segment_length: int) -> np.array:
+    """
+    Calculates energy of the audio segment. Normalised with segment legth.
+    """
     energy = []
     for i in range(int(len(signal)/segment_length)):
         segment = signal[i*segment_length:(i+1)*segment_length]# try except error ...
@@ -14,9 +17,15 @@ def energy_calc(signal, segment_length):
             print(i)
     return energy
 
-# Extract characteristics from signal
-def preprocess_signal(filename, short_term_length=0.020, short_term_overlap=0,\
-                      medium_term_length=1, medium_term_overlap=0.020):
+# Preprocess signal
+def preprocess_signal(filename: str, short_term_length:float=0.020, short_term_overlap:float=0,\
+                      medium_term_length:float=1, medium_term_overlap:float=0.020) -> np.array:
+    """
+    Preprocessing of the audiofile to get 28 coeficients after three steps:
+    - Short term analysis: segmentation of audio to get energy and 13 MFCCs per segment. 
+    - Medium term analysis: segmentation of audio to get mean and standard deviation per segment.
+    - Long term analysis: mean of the medium term values per segment.
+    """
     
     # Import audio signal
     sr, signal = read(filename)
@@ -28,7 +37,7 @@ def preprocess_signal(filename, short_term_length=0.020, short_term_overlap=0,\
         signal = signal[::sr_ratio,0]
     except IndexError:
         signal = signal[::sr_ratio]
-    sr = sr_objective
+    sr = sr_objective    
 
     # Normalise
     signal = signal.astype(np.float32)
@@ -50,11 +59,7 @@ def preprocess_signal(filename, short_term_length=0.020, short_term_overlap=0,\
     
     # SHORT TERM ANALYSIS
     # Calculate MFCCs for short term
-    mfcc_st = librosa.feature.mfcc(y=signal,
-                                   sr=sr, 
-                                   n_fft=n_fft_st,
-                                   n_mfcc=13, 
-                                   hop_length=hop_length_st)
+    mfcc_st = librosa.feature.mfcc(y=signal, sr=sr, n_fft=n_fft_st, n_mfcc=13, hop_length=hop_length_st)
     mfcc_st = mfcc_st[:,:len(energy)]
     coefficients_st = np.vstack((mfcc_st, energy))
 
@@ -84,17 +89,23 @@ def preprocess_signal(filename, short_term_length=0.020, short_term_overlap=0,\
 
     return parameters_lt
 
-# Extract label from directory name
-def get_label(filename):
+# Get labels from directories
+def get_label(filename:str) -> str:
+    """
+    Assign label from directory name.
+    """
     label = filename.split("/")[-2]
     return label
 
-# Join characteristics with label
-def add_label(filename):
+# Merge characteristics and labels
+def add_label(filename:str) -> np.array:
+    """
+    Add label to numpy array with 28 characteristics.
+    """
     coefficients = preprocess_signal(filename)
     label = np.array(get_label(filename))
     return np.hstack((coefficients, label))
 
-# Merge numpy array with characteristics and label 
-def add_label_arrays(x, y):
+# Merge characteristics and labels from numpy arrays
+def add_label_arrays(x:np.array, y:np.array) -> np.array:
     return np.hstack((x, y))
